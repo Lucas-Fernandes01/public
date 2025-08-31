56cbb75d06ec6e1ca215656bc1b1450061ad84a3
CaioXys <caiofer0515@gmail.com>
2025-08-31 12:21:11 -0300

    Tirando do cachê arquivos para não serem visualizados.

--- conexao.php
+++
@@ -1,9 +0,0 @@
-<?php
-define('HOST', 'localhost');
-define('USER', 'root');
-define('PASS', '');
-define('DB', 'acai');
-
-$conn = mysqli_connect(HOST, USER, PASS, DB) or die ("Não foi possível conectar");
-
-?>
